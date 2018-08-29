cf9e562df1369d277cf71004b8fe7349726e7f86
bsruthi <bsruthi@DIN80000651.corp.capgemini.com>
2018-08-29 13:39:39 +0530

    basicBeans

---
+++ capstore/src/main/java/com/cg/capstore/CapstoreApplication.java
@@ -0,0 +1,12 @@
+package com.cg.capstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoreApplication.class, args);
+	}
+}
